b15fcb50ac58ffb87cf77652baf0f558d1c2f3b0
Mike Skelton <skeltonmr43@gmail.com>
2017-09-09 16:42:52 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :rosters
   resources :users
-  resources :players
+  resources :players, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
